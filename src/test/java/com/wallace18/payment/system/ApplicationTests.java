8f31bc75582e024bf74cd9a25e9c271eb9ab6cbb
Wallace Patrick <patrickwallace621@gmail.com>
2024-01-24 14:35:43 -0300

    Project created (Docker sql - file.properties)

---
+++ src/test/java/com/wallace18/payment/system/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallace18.payment.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
